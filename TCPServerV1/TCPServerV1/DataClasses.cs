67c62078dd20d03fd455fe1fe9158c7f4693582c
FamousDirector <jamesadcameron111@gmail.com>
2016-12-22 23:28:28 -0400

    added function to parse controller data

---
+++ TCPServerV1/TCPServerV1/DataClasses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TCPServerV1
+{
+    static class DataClasses
+    {
+        public class ControllerData
+        {
+            public string UID { get; set; }
+            public int RelayState { get; set; }
+            public int Temperature { get; set; }
+            public int Power { get; set; }
+        }
+    }
+}
---
+++ TCPServerV1/TCPServerV1/Messaging.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TCPServerV1
+{
+    static class Messaging
+    {
+        static public DataClasses.ControllerData ParseMessageData(string message)
+        {
+            //ex data : <UID = JAMESESP8266>< Relay = 0 >< Temperature = 85 >< Power = 88 >< EOM >
+            DataClasses.ControllerData newcontrollerdata = new DataClasses.ControllerData();
+
+            char[] delimiterChars = { '<', '>',};
+            string[] rawdatastrings = message.Split(delimiterChars);
+
+            //Set UID
+            string rawuid = Array.FindLast(rawdatastrings, s => s.StartsWith("UID"));
+            newcontrollerdata.UID = rawuid.Remove(0, rawuid.LastIndexOf('=') + 1);
+
+            //Set RelayState
+            string rawrelaystate = Array.FindLast(rawdatastrings, s => s.StartsWith("Relay"));
+            newcontrollerdata.RelayState = int.Parse(rawrelaystate.Remove(0, rawrelaystate.LastIndexOf('=') + 1));
+
+            //Set Temperature
+            string rawtemperature = Array.FindLast(rawdatastrings, s => s.StartsWith("Temperature"));
+            newcontrollerdata.Temperature = int.Parse(rawtemperature.Remove(0, rawtemperature.LastIndexOf('=') + 1));
+
+            //Set Power Usage
+            string rawpower = Array.FindLast(rawdatastrings, s => s.StartsWith("Power"));
+            newcontrollerdata.Power = int.Parse(rawpower.Remove(0, rawpower.LastIndexOf('=') + 1));
+
+            return newcontrollerdata;
+        }
+        static public void StoreControllerData(DataClasses.ControllerData controllerdata)
+        {
+            
+            return;
+        }
+        static public string CreateReplyMessage(DataClasses.ControllerData controllerdata)
+        {
+            string replymessage = "This is a test reply";
+
+            return replymessage;
+        }
+    }
+}
